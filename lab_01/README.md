9d839a1a3fc2e7bb5f1ec3a0f3177a93d1c4b2f6
Nedoluzhko Denis <denisnedoluzhko1902@gmail.com>
2021-09-11 00:32:56 +0300

    lab_01:user, db, tables creation added

---
+++ lab_01/README.md
@@ -0,0 +1,19 @@
+## Запуск
+
+Запуск docker контейнера
+
+```sh
+docker run --name mypostgres -e POSTGRES_PASSWORD=mysecret -d -p 5432:5432 postgres
+```
+
+Создание пользователя, базы данных и передача прав на бд пользователю
+
+```sh
+psql -h localhost -p 5432 -U postgres -W -f create_user.sql
+```
+
+Создание таблиц
+
+```sh
+psql -h localhost -p 5432 -U deniska -d musicdb -W -f create_tables.sql
+```
---
+++ lab_01/create_tables.sql
@@ -0,0 +1,81 @@
+CREATE TABLE label (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    founder TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    country TEXT NOT NULL,
+    about TEXT
+);
+
+CREATE TABLE band (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    country TEXT NOT NULL,
+    about TEXT
+);
+
+CREATE TABLE med_band_label (
+    id INTEGER PRIMARY KEY,
+    id_band INTEGER NOT NULL,
+    id_label INTEGER NOT NULL,
+    c_start DATE NOT NULL,
+    c_end DATE NOT NULL,
+    FOREIGN KEY (id_band) REFERENCES band(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_label) REFERENCES label(id) ON DELETE CASCADE
+);
+
+CREATE TABLE song (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    duration INTERVAL NOT NULL,
+    about TEXT
+);
+
+CREATE TABLE med_band_song (
+    id INTEGER PRIMARY KEY,
+    id_band INTEGER NOT NULL,
+    id_song INTEGER NOT NULL,
+    FOREIGN KEY (id_band) REFERENCES band(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_song) REFERENCES song(id) ON DELETE CASCADE
+);
+
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    about TEXT
+);
+
+CREATE TABLE med_album_song (
+    id INTEGER PRIMARY KEY,
+    id_album INTEGER NOT NULL,
+    id_song INTEGER NOT NULL,
+    FOREIGN KEY (id_album) REFERENCES album(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_song) REFERENCES song(id) ON DELETE CASCADE
+);
+
+CREATE TABLE songrating (
+    id INTEGER PRIMARY KEY,
+    rating INTEGER NOT NULL,
+    time TIME NOT NULL,
+    id_song INTEGER NOT NULL
+);
+
+CREATE TABLE listener (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    gender TEXT,
+    birth DATE,
+    email TEXT NOT NULL,
+    pwd_hash TEXT NOT NULL
+);
+
+CREATE TABLE med_listener_songrating (
+    id INTEGER PRIMARY KEY,
+    id_listener INTEGER NOT NULL,
+    id_songrating INTEGER NOT NULL,
+    FOREIGN KEY (id_listener) REFERENCES listener(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_songrating) REFERENCES songrating(id) ON DELETE CASCADE
+);
